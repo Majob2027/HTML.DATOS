73ab573456c347a8cdd6b769ed7f443de33e5069
Maria <mbastilla2007@gmail.com>
2024-04-17 11:27:56 -0500

    feat: :sparkles: Objetos

---
+++ Dia4/Objetos.js
@@ -0,0 +1,17 @@
+// Carro //
+
+
+var Car = new Object();
+Car.make = "Mazda";
+Car.model = "3";
+Car.year = 2019;
+
+
+var myCar = {
+    make: "Mazda",
+    model: "3",
+    year: 2019,
+  };
+  
+
+// Carro //
