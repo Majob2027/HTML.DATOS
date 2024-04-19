6eddd64de644de843d68d83a45490ca24fe6367f
Maria <mbastilla2007@gmail.com>
2024-04-19 08:47:58 -0500

    feat: :sparkles: Ejercicio completado

--- Dia6/Ejercicio5.js
+++ Dia6/Ejercicio5.js
@@ -1,49 +1,34 @@
-class CuentaBancaria {
-    constructor(saldo, Ncuenta) {
-        this.Ncuenta = Ncuenta;
-        this.saldo = saldo;
-    }
-
-    depositar(cantidad) {
-        if (cantidad > 0) {
-            this.saldo += cantidad;
-            console.log("Depósito realizado. Nuevo saldo: " + this.saldo);
-        } else {
-            console.log("La cantidad a depositar debe ser mayor que 0.");
-        }
-    }
-
-    retirar(retiro) {
-        if (retiro > 0 && retiro <= this.saldo) { 
-            this.saldo -= retiro;
-            console.log("Retiro realizado. Nuevo saldo: " + this.saldo);
-        } else {
-            console.log("No tienes fondos suficientes para retirar esa cantidad de dinero.");
-        }
+class Forma {
+    calcularArea() {
+        console.log("");
     }
 }
 
-let Ncuenta = prompt("Ingrese su número de cuenta: ");
-let saldoInicial = parseFloat(prompt("Ingrese saldo inicial: "));
-let cuenta = new CuentaBancaria(saldoInicial, Ncuenta);
+class Circulo extends Forma {
+    constructor(radio) {
+        super();
+        this.radio = radio;
+    }
 
-while (true) {
-    console.log("¿Qué deseas hacer?");
-    console.log("1 = Depositar dinero");
-    console.log("2 = Retirar dinero");
-    console.log("3 = Salir");
-
-    let decision = parseInt(prompt("Ingrese el número de la operación que desea realizar: "));
-
-    if (decision === 1) {
-        let cantidad = parseFloat(prompt("Ingrese la cantidad de dinero que desea depositar: "));
-        cuenta.depositar(cantidad);
-    } else if (decision === 2) {
-        let retiro = parseFloat(prompt("Ingrese la cantidad de dinero que desea retirar: "));
-        cuenta.retirar(retiro);
-    } else if (decision === 3) {
-        break;
-    } else {
-        console.log("Operación no válida. Por favor, ingrese 1 para depositar dinero, 2 para retirar dinero o 3 para salir.");
+    calcularArea() {
+        return Math.PI * this.radio ** 2;
     }
 }
+
+class Triangulo extends Forma {
+    constructor(base, altura) {
+        super();
+        this.base = base;
+        this.altura = altura;
+    }
+
+    calcularArea() {
+        return 0.5 * this.base * this.altura;
+    }
+}
+
+const circulo = new Circulo(8);
+console.log("Área del círculo:", circulo.calcularArea());
+
+const triangulo = new Triangulo(10, 50);
+console.log("Área del triángulo:", triangulo.calcularArea());
