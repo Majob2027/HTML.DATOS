0d61f64cb976af24fbd24cf09c5611eaa47a23a7
Majob2027 <mbastilla2007@gmail.com>
2024-04-29 21:23:58 -0500

    feat: :sparkles: continuacion de dia 12

--- Dia12/script.js
+++ Dia12/script.js
@@ -1,3 +1,6 @@
+let cartaSeleccionada = null;
+let cartasMostradas = [];
+
 // Función para cargar las cartas desde la API
 async function cargarCartas() {
     try {
@@ -24,6 +27,44 @@ function mostrarCartas(cartas) {
     });
 }
 
+// Función para seleccionar una carta
+function seleccionarCarta(index) {
+    const cartaActual = cartasMostradas[index].image;
+
+    if (cartaSeleccionada === null) {
+        // Si es el primer clic, guardar la URL de la carta seleccionada
+        cartaSeleccionada = cartaActual;
+        document.getElementById(`card${index}`).classList.add('seleccionada');
+    } else {
+        // Si es el segundo clic, comparar las URLs de las cartas seleccionadas
+        if (cartaSeleccionada === cartaActual) {
+            // Cartas iguales, se ha ganado el juego
+            alert('¡Has ganado el juego!');
+            reiniciarJuego();
+        } else {
+            // Cartas diferentes, mostrar mensaje de intentar nuevamente
+            alert('¡Inténtalo de nuevo!');
+            document.getElementById(`card${index}`).classList.remove('seleccionada');
+            document.querySelectorAll('.card').forEach(card => {
+                card.classList.remove('seleccionada');
+            });
+        }
+        // Reiniciar la carta seleccionada para la próxima comparación
+        cartaSeleccionada = null;
+    }
+
+    console.log("Carta seleccionada: ", cartaActual);
+}
+
+
+
+
+// Función para reiniciar el juego
+function reiniciarJuego() {
+    cartaSeleccionada = null;
+    cargarCartas();
+}
+
 // Función para reorganizar aleatoriamente las cartas
 function reorganizarCartas() {
     // Obtener una lista de índices aleatorios únicos
@@ -42,13 +83,18 @@ function reorganizarCartas() {
     mostrarCartas(cartasReorganizadas);
 }
 
-
 cargarCartas();
 
-setTimeout(() => {
-    reorganizarCartas();
+// Función para reiniciar el juego
+function reiniciarJuego() {
+    cartaSeleccionada = null;
+    cargarCartas();
     setTimeout(() => {
         reorganizarCartas();
-        setTimeout(reorganizarCartas, 1000);
-    }, 1000);
-}, 5000);
+        setTimeout(() => {
+            reorganizarCartas();
+            setTimeout(reorganizarCartas, 1000);
+        }, 1000);
+    }, 5000);
+}
+
