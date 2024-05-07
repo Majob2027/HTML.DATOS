833893d2c0c1358a6646ca350fc19934e7e9d8fa
Maria <mbastilla2007gmail.com>
2024-05-07 13:14:03 -0500

    feat: :sparkles: terminacion de quiz

--- Dia17 - quiz/script.js
+++ Dia17 - quiz/script.js
@@ -1,7 +1,7 @@
-function Getuser() {
+function GetUser() {
     const apiUrl = `https://randomuser.me/api/`;
 
-    console.log("hola")
+    console.log("Hola");
 
     fetch(apiUrl)
         .then(response => {
@@ -11,21 +11,26 @@ function Getuser() {
             return response.json();
         })
         .then(data => {
-            let firstName = data.results[0].name.first
-            let lastname = data.results[0].name.last
-            let email = data.results[0].email
-            let date = data.results[0].date
-            let numerocalle = data.results[0].date
-            let nombredecalle = data.nombredecalle;
-            let phone = data.phone;
-            let password = data.password;
-            let image = data.image;
-`
-            document.getElementById('firstName').textContent = ${firsName}-`
-            document.getElementById('lastname').textContent = `${lastname}`;
-            document.getElementById('pokemonImage').src = pokemonAnimatedSpriteUrl;
+            let firstName = data.results[0].name.first;
+            let lastName = data.results[0].name.last;
+            let email = data.results[0].email;
+            let date = data.results[0].dob.date;
+            let streetNumber = data.results[0].location.street.number;
+            let streetName = data.results[0].location.street.name;
+            let phone = data.results[0].phone;
+            let image = data.results[0].picture.large;
+
+            document.getElementById('firstName').textContent = firstName;
+            document.getElementById('lastName').textContent = lastName;
+            document.getElementById('email').textContent = email;
+            document.getElementById('date').textContent = date;
+            document.getElementById('street').textContent = `${streetNumber} ${streetName}`;
+            document.getElementById('phone').textContent = phone;
+            document.getElementById('image').src = image;
         })
         .catch(error => {
             console.error('Ocurrió un problema con tu función: ', error);
         });
 }
+
+GetUser();
